f8ba4cb5d05b10bb289d6639dbbbb1bcba5f5a2c
Abel Majadibodu <Abeljackson33@gmail.com>
2023-11-04 11:16:14 +0200

    footer fix

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { motion } from "framer-motion";
+import classnames from "classnames";
 
 const Footer: FC = () => {
   return (
     <motion.div
-      className="flex flex-row justify-center items-center h-[30px] sm:h-[50px] border-t border-red-900 py-2 px-8"
+      className={classnames("fixed bottom-0 w-full",
+        "flex flex-row justify-center items-center h-[30px] sm:h-[50px] border-t border-red-900 py-2 px-8")}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
